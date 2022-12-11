230f4541b92900504ea1435c6dc83bef56d49d36
Gaby <gabriela.diaz.rousseau@mi.unc.edu.ar>
2022-12-11 13:51:27 -0300

    Relacionando apps

--- AppNotas/apps/notes/models.py
+++ AppNotas/apps/notes/models.py
@@ -1,10 +1,36 @@
 from django.db import models
+from apps.users.models import User
+import random
 
 # Create your models here.
 
 class Notes (models.Model):
-    title=models.CharField(max_length=50, verbose_name='Title')
-    description=models.CharField(max_length=200, verbose_name='Description')
-    state=models.CharField(max_length=50, verbose_name='State')
-    creation_date=models.DateField(verbose_name='Creation_Date')
-    deadline=models.DateField(verbose_name='Deadline')
+
+    def create_new_ref_number():
+        not_unique = True
+        while not_unique:
+            unique_ref = random.randint(1000000000, 9999999999)
+            if not Notes.objects.filter(reference_number=unique_ref):
+                not_unique = False
+        return str(unique_ref)
+
+    #atributos
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    reference_number=models.CharField(max_length=10, blank=True, editable=False, unique=True, default=create_new_ref_number, verbose_name='Número de referencia')
+    title=models.CharField(max_length=50, verbose_name='Título')
+    description=models.CharField(max_length=200, verbose_name='Descripción')
+    state=models.CharField(max_length=50, verbose_name='Estado')
+    creation_date=models.DateField(verbose_name='Fecha de creación')
+    deadline=models.DateField(verbose_name='Fecha de finalización')
+
+
+    
+    class Meta:
+        verbose_name = 'Nota'
+        verbose_name_plural = 'Notas'
+    
+    def __str__(self) -> str:
+        return self.reference_number
--- AppNotas/apps/users/models.py
+++ AppNotas/apps/users/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+
 # Create your models here.
 
 class User (models.Model):
-    name=models.CharField(max_length=50, verbose_name='Name')
-    surname=models.CharField(max_length=50, verbose_name='Surname')
-    username=models.CharField(max_length=50, unique=True, verbose_name='Username')
-    password=models.CharField(max_length=30, verbose_name='Password')
+
+    # atributos 
+
+    name=models.CharField(max_length=50, verbose_name='Nombre')
+    last_name=models.CharField(max_length=50, verbose_name='Apellido')
+    username=models.CharField(max_length=50, unique=True, verbose_name='Nombre de usuario')
+    password=models.CharField(max_length=30, verbose_name='Contraseña')
     email=models.EmailField(unique=True, verbose_name='Email')
+
+    class Meta:
+            verbose_name = 'Usuario'
+            verbose_name_plural = 'Usuarios'
+
+    def __str__(self):
+            return f'{self.name} {self.last_name}'
+    
