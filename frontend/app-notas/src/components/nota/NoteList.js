4e8fdd1ba92e698628f557fa48e9bac637d8737c
SalvadorCasas <salvadorcasas11@gmail.com>
2022-12-14 16:54:59 -0300

    Logica componente nota

--- frontend/app-notas/src/components/nota/NoteList.js
+++ frontend/app-notas/src/components/nota/NoteList.js
@@ -1,22 +1,37 @@
-import React , { useState } from 'react';
+import React , { useEffect, useState } from 'react';
+import * as NoteServer from './NoteServer';
 
-const NoteList=()=>{
+const NoteList = () => {
 
-  const initialState = [
+  /*const initialState = [
     {
       id:9,
       title:"Primera Nota",
       description:"Esta es la primera nota que creamos",
     },
-  ];
+  ];*/
 
 //CREA ATRIBUTO notes y METODO setNotes
-const [notes,setNotes]=useState(initialState);
+  const [notes,setNotes]= useState([]);
+
+  const listNotes= async () => {
+    try {
+      const res = await NoteServer.listNotes();
+      const data = await res.json();
+      setNotes(data.notes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    listNotes();
+  }, []);
 
   return (
     <div>
-      {notes.map((notes)=>(
-        <h2>{notes.title}</h2>
+      {notes.map((note) => (
+        <h2>{note.title}</h2>
       ))}
     </div>
   );
